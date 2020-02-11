5aa16ce1715beca350f40a1b8cf78d7d43d76799
pavlenty <impetus@yandex.ru>
2020-02-11 23:51:30 +0300

    Add button

--- app/src/main/java/ru/pavlenty/recyclerviewexample/MainActivity.java
+++ app/src/main/java/ru/pavlenty/recyclerviewexample/MainActivity.java
@@ -9,18 +9,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
     private MyRecyclerViewAdapter adapter;
-
+    private ArrayList<String> animalNames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> animalNames = new ArrayList<>();
+
+
+        animalNames = new ArrayList<>();
         animalNames.add("Horse");
         animalNames.add("Cow");
         animalNames.add("Camel");
@@ -37,6 +42,20 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         recyclerView.addItemDecoration(dividerItemDecoration);
 
         recyclerView.setAdapter(adapter);
+
+        ExtendedFloatingActionButton btn_add = findViewById(R.id.floating_button_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<String> items = new ArrayList<>();
+                items.add("Pig");
+                items.add("Chicken");
+                items.add("Dog");
+                int insertIndex = 2;
+                animalNames.addAll(insertIndex, items);
+                adapter.notifyItemRangeInserted(insertIndex, items.size());
+            }
+        });
     }
 
     @Override
